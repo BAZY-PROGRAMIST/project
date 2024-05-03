254f4bf3b2895d08b772150e6d4b317fb499de08
Epishko <timyrcivko2009@gmail.com>
2024-05-03 19:22:08 +0300

    rfgregdd

--- load.py
+++ load.py
@@ -2,5 +2,7 @@ import pygame
 from script import load_image
 block_image = pygame.image.load('image/block/block.jpg').convert_alpha()
 water_image = pygame.image.load('image/block/water.jpg').convert_alpha()
-player_image = load_image('image/player/run')
+player_idle = load_image('image/player/idle')
+player_run = load_image('image/player/run')
+player_atk = load_image('image/player/attack')
 box_image = pygame.image.load('image/block/box.png').convert_alpha()
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def restart():
     water_group = pygame.sprite.Group()
     camera_group = pygame.sprite.Group()
     box_group = pygame.sprite.Group()
-    player = Player(player_image, (300, 300))
+    player = Player((300, 300),{'idle':player_idle, 'run':player_run, 'atk':player_atk})
     player_group = pygame.sprite.Group()
     player_group.add(player)
 
@@ -127,9 +127,10 @@ class Water(pygame.sprite.Sprite):
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, image, pos):
+    def __init__(self, pos, image_lists):
         pygame.sprite.Sprite.__init__(self)
-        self.image = image
+        self.image_lists = image_lists
+        self.image = self.image_lists['idle'][0]
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]+200
@@ -138,50 +139,83 @@ class Player(pygame.sprite.Sprite):
         self.on_ground = True
         self.frame = 0
         self.timer_anime = 0
-        self.anime = False
+        self.anime_idle = False
+        self.anime_run = False
+        self.anime_atk = False
         self.key = pygame.key.get_pressed()
         self.dir = 'right'
         self.timer_attack = 0
+        self.jump = False
+        self.jump_step = -20
+        self.flag_damage = False
     def update(self):
+        self.animation()
         self.move()
-        self.jump()
-        self.anime()
+        self.atk()
         self.key = pygame.key.get_pressed()
 
-    def anime(self):
-        if self.anime:
-            self.timer_anime += 1
-            if self.timer_anime / FPS > 0.1:
-                if self.frame == len(player_image) - 1:
-                    self.frame = 0
-                else:
-                    self.frame += 1
-                self.timer_anime = 0
+    def animation(self):
+        self.timer_anime += 2
+        if self.timer_anime / FPS > 0.1:
+            if self.frame == len(self.current_list_image) - 1:
+                self.frame = 0
+                if self.anime_atk:
+                    self.current_list_image = player_idle
+                    self.anime_atk = False
+                    self.anime_idle = True
+            else:
+                self.frame += 1
+            self.timer_anime = 0
+        if self.anime_idle:
+            self.current_list_image = self.image_lists['idle']
+        if self.anime_run:
+            self.current_list_image = self.image_lists['run']
+        if self.anime_atk:
+            self.current_list_image = self.image_lists['atk']
+        try:
+            if self.dir == 'right':
+                self.image = self.current_list_image[self.frame]
+            else:
+                self.image = pygame.transform.flip(self.current_list_image[self.frame], True, False)
+        except:
+            self.frame = 0
     def move(self):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_d]:
-            self.rect.x += self.speed
-            self.image = player_image
-            if self.rect.right > 1000:
-                self.rect.right = 1000
-                camera_group.update(-self.speed)
+        if self.key[pygame.K_d]:
+            self.rect.x += 2
+            self.anime_idle = False
+            if not self.anime_atk:
+                self.anime_run = True
+        elif self.key[pygame.K_a]:
+            self.rect.x -= 2
+            self.anime_idle = False
+            if not self.anime_atk:
+                self.anime_run = True
+        else:
+            if not self.anime_atk:
+                self.anime_idle = True
+            self.anime_run = False
 
-        elif key[pygame.K_a]:
-            self.image = pygame.transform.flip(player_image, True, False)
-            self.rect.x -= self.speed
-            if self.rect.left > 1000:
-                self.rect.left = 1000
-                camera_group.update(-self.speed)
+
         if pygame.sprite.spritecollide(self, water_group, False):
             self.kill()
     def jump(self):
-        if self.key[pygame.K_w] and self.on_ground:
-            self.velocity_y = -25
-            self.on_ground = False
-        self.rect.y += self.velocity_y
-        self.velocity_y += 1
-        if self.velocity_y > 10:
-            self.velocity_y = 10
+        if self.key[pygame.K_w]:
+            self.jump = True
+        if self.jump:
+            if self.jump_step <= 20:
+                self.rect.y += self.jump_step
+                self.jump_step += 1
+            else:
+                self.jump = False
+                self.jump_step = -20
+    def atk(self):
+        if self.key[pygame.K_SPACE] and not self.anime_atk:
+            self.frame = 0
+            self.anime_atk = True
+            self.anime_idle = False
+            self.anime_run = False
+            self.flag_damage = True
+
 
 
 
